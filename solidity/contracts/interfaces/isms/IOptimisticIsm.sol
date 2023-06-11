94050c03486cf068653dfd02fa571d41ac6bc9fa
Muhammed Eren <info@sunbot.co.uk>
2023-06-11 23:51:25 +0100

    functions explanations and dummy completeDeliveryProcess function implementation

--- solidity/contracts/interfaces/isms/IOptimisticIsm.sol
+++ solidity/contracts/interfaces/isms/IOptimisticIsm.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 import {IInterchainSecurityModule} from "../IInterchainSecurityModule.sol";
 
 interface IOptimisticIsm is IInterchainSecurityModule {
-    function deliverMessage(bytes calldata _metadata, bytes calldata _message) external returns (bool);
+    function triggerMessageDelivery(bytes calldata _metadata, bytes calldata _message) external returns (bool);
 	function preVerify(bytes calldata _metadata, bytes calldata _message) external returns (bool);
 	function markFraudulent(address _submodule) external;
 	function getSubmodule(bytes calldata _message) external view returns (IInterchainSecurityModule);
--- solidity/contracts/isms/Optimistic/OptimisticISM.sol
+++ solidity/contracts/isms/Optimistic/OptimisticISM.sol
@@ -5,7 +5,6 @@ import {IOptimisticIsm} from "../../interfaces/isms/IOptimisticIsm.sol";
 import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
 import {StaticMOfNAddressSetFactory} from "../../libs/StaticMOfNAddressSetFactory.sol";
 
-
 abstract contract OptimisticISM is IOptimisticIsm {
 
     struct FraudWindow {
@@ -13,13 +12,12 @@ abstract contract OptimisticISM is IOptimisticIsm {
         bool preVerified;
         mapping(address => bool) compromisedSubmodules;
     }
-    
     mapping(bytes32 => FraudWindow) public fraudWindows;
     IInterchainSecurityModule private _submodule;
     address private owner;
     address[] public watchers;
     mapping(address => bool) public isWatcher;
-    StaticMOfNAddressSetFactory private addressSetFactory;
+    address  private _addressSetFactory;
 
     
     modifier onlyOwner() {
@@ -27,13 +25,23 @@ abstract contract OptimisticISM is IOptimisticIsm {
         _;
     }
 
-    constructor(StaticMOfNAddressSetFactory _addressSetFactory) {
-        addressSetFactory = _addressSetFactory;
+    constructor(address addressSetFactory) {
+        _addressSetFactory = addressSetFactory;
         owner = msg.sender;
     }
 
+            /**
+            *Important Requires an additional step to deploy and obtain the StaticMOfNAddressSetFactory contract address before deploying the OptimisticISM contract.
+     * @notice  allows the contract owner to configure the watchers by providing an array of watcher addresses and a threshold value. 
+     It deploys a new instance of the StaticMOfNAddressSet contract using the StaticMOfNAddressSetFactory 
+     and passes the provided watcher addresses and threshold to the factory's deploy function. 
+     The deployed StaticMOfNAddressSet contract's address is stored, and the watchers array and isWatcher mapping are updated accordingly.
+     * @param _watchers passes the provided watcher addresses.
+     * @param _threshold thresholds to the factory's deploy function.
+     */
     function configureWatchers(address[] calldata _watchers, uint8 _threshold) external onlyOwner {
-        // Deploy a StaticMOfNAddressSet instance using the factory
+        // Deploy a StaticMOfNAddressSet instance using the provided addressSetFactory
+        StaticMOfNAddressSetFactory addressSetFactory = StaticMOfNAddressSetFactory(_addressSetFactory);
         address addressSet = addressSetFactory.deploy(_watchers, _threshold);
 
         // Set the deployed addressSet as the new set of watchers
@@ -52,7 +60,13 @@ abstract contract OptimisticISM is IOptimisticIsm {
 
     event WatchersConfigured(address indexed addressSet, address[] watchers, uint8 threshold);
 
-    function deliverMessage(bytes calldata _metadata, bytes calldata _message) external override returns (bool)  {
+        /**
+     * @notice Starts the fraud window. both _metadata and _message params required to be able to have a messageHash for next functions on queryiying the FraudWindow
+     * @param _metadata The metadata associated with the message.
+     * @param _message The message to be delivered.
+     * @return True if the fraud window has started
+     */
+    function triggerMessageDelivery(bytes calldata _metadata, bytes calldata _message) external override returns (bool)  {
 
         bytes32 messageHash = calculateMessageHash(abi.encodePacked(_metadata, _message));
     
@@ -68,11 +82,29 @@ abstract contract OptimisticISM is IOptimisticIsm {
         // Set the endTimestamp of the FraudWindow
         fraudWindow.endTimestamp = block.timestamp + fraudWindowDuration();
         
-        //Rest of delivery related implementation if required...
-        // ...
         return true;
     }
 
+      /**
+     * @notice Rest of delivery related implementation if there are non-implemented use-cases
+     * @return True if the rest of delivery implementations work well
+     */
+    function completeDeliveryProcess() internal pure returns (bool) {
+        
+        bool isDeliveryOK;
+        //Rest of delivery related implementation if required...
+        // 
+
+        return isDeliveryOK;
+
+    }
+
+        /**
+     * @notice Verifies a message using the submodule
+     * @param _metadata The metadata associated with the message.
+     * @param _message The message to be verified.
+     * @return True if the message is verified and the fraud window has elapsed, false otherwise.
+     */
     function preVerify(bytes calldata _metadata, bytes calldata _message) external override returns (bool) {
         bytes32 messageHash = calculateMessageHash(abi.encodePacked(_metadata, _message));
 
@@ -84,14 +116,20 @@ abstract contract OptimisticISM is IOptimisticIsm {
         // Perform pre-verification using the configured submodule
         bool isVerified = _submodule.verify(_metadata, _message);
 
-        if (!isVerified) {
-            //Depens on an another use-case mark the submodule as compromised if pre-verification fails
-            //markFraudulent(address(_submodule));
-        }
+        //function may call more than 1 so relatedWindow will hold last preVerified state
+        if (isVerified) { relatedWindow.preVerified = true; }
+        else { relatedWindow.preVerified = false; }
 
         return isVerified;
     }
 
+
+        /**
+     * @notice Verifies a message using the submodule and checks if the submodule has been compromised and checks if the fraud window has elapsed.
+     * @param _metadata The metadata associated with the message.
+     * @param _message The message to be verified.
+     * @return True if the message is verified and the fraud window has elapsed, false otherwise.
+     */
     function verify(bytes calldata _metadata, bytes calldata _message) external view virtual override returns (bool) {
         bytes32 messageHash = calculateMessageHash(abi.encodePacked(_metadata, _message));
         FraudWindow storage fraudWindow = fraudWindows[messageHash];
@@ -111,9 +149,16 @@ abstract contract OptimisticISM is IOptimisticIsm {
             return false;
         }
 
+        completeDeliveryProcess();
+
         return true;
     }
 
+    /**
+     * @notice An internal function; first finds relatedFraudWindows with messageHash and checks last preVerified state
+     * @param messageHash first calculated on triggerMessageDelivery function. helps to find related Fraud-Window
+     * @return True if the message last pre-verification ok, false otherwise.
+     */
     function isPreVerified(bytes32 messageHash) internal view returns (bool) {
         return (fraudWindows[messageHash].preVerified);
     }
@@ -122,6 +167,9 @@ abstract contract OptimisticISM is IOptimisticIsm {
         fraudWindows[calculateMessageHash(msg.data)].compromisedSubmodules[submodule] = true;
     }
 
+        /**
+     * @notice Only Contract Owner could configure submodule accordingly
+     */
     function configureSubmodule(IInterchainSecurityModule submodule) external onlyOwner {
         _submodule = submodule;
     }
